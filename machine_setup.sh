2c13941b9fe04c6f1b58d8da4065325ce43a5b1d
Adam Dymitruk <adam@eventsourcing.com>
2024-11-01 17:38:36 -0700

    main script pauses after output

--- machine_setup.sh
+++ machine_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script is used for machine setup
-
+declare -A menu_items
 while true; do
   clear
   declare -A scripts
@@ -8,12 +8,19 @@ while true; do
   for script in machine_setup/*.sh; do
     scripts[$index]=$script
     script_name=$(basename $script)
-    if bash "$script" check > /dev/null; then
-      echo -e "$index. \e[32m\u2713\e[0m $script_name"
+    if bash "$script" check; then
+      menu_items[$index]="$index. \e[32m\u2713\e[0m $script_name"
     else
-      echo -e "$index. \e[30m\u25CF\e[0m $script_name"
+      menu_items[$index]="$index. \e[30m\u25CF\e[0m $script_name"
     fi
     index=$((index+1))
+  done
+  echo "done checking scripts. press enter to continue"
+  read -p ""
+  clear
+  echo "Select an option:"
+  for item in "${menu_items[@]}"; do
+    echo -e "$item"
   done
   read -p "Enter the number of the script you want to run, or 'q' to quit: " choice
   if [[ "$choice" == "q" ]]; then
@@ -21,6 +28,8 @@ while true; do
   fi
   if [[ -n "${scripts[$choice]}" ]]; then
     bash "${scripts[$choice]}"
+    echo "Press Enter to continue"
+    read -p ""
   else
     echo "Invalid choice. Please try again."
   fi
